dbb54c9b9886f0caae7618a68059f12908910492
unknown <vishaliscoding27@gmail.com>
2023-09-20 17:14:59 +0530

    15th commit

--- controller/messageController.js
+++ controller/messageController.js
@@ -3,12 +3,10 @@ const handleMeaasge = async (req, res) => {
 	const { senderId, receiverId } = req.body;
 	console.log(req.body);
 	if (senderId != null && receiverId != null) {
-		const messageList = await message.find({
-			$or: [
-				{ sender: senderId, receiver: receiverId }, 
-				{ sender: receiverId, receiver: senderId }, 
-			],
-		});
+		let messageList =message.find({
+				$and: [{ sender: senderId }, { receiver: receiverId }],
+			})
+			.sort({ createdAt: 1 });
         res.status(200).send({
             messages:messageList
         });
