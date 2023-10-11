417c5be3979ec864e1bfca0be73d69f972cf64b9
unknown <vishaliscoding27@gmail.com>
2023-10-11 15:41:17 +0530

    3rd commit

--- routes/getUserContactRoute.js
+++ routes/getUserContactRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const getuserContactController = require("../controller/getUserContactController");
-router.get("/", getuserContactController.getContact);
+router.post("/", getuserContactController.getContact);
 module.exports = router;
